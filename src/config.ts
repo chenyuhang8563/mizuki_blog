98fa210fb432aee59a8a91f095b910a6bf037d6c
xc <1608553627@qq.com>
2026-03-11 02:22:41 +0800

    test

--- src/config.ts
+++ src/config.ts
@@ -24,7 +24,7 @@ const SITE_TIMEZONE = 8; //设置你的网站时区 from -12 to 12 default in UT
 export const siteConfig: SiteConfig = {
 	title: "xcpolping的小站",
 	subtitle: "everything is better than nothing",
-	siteURL: "https://mizuki.mysqil.com/", // 请替换为你的站点URL，以斜杠结尾
+	siteURL: "https://mizuki-blog-rust.vercel.app/", // 请替换为你的站点URL，以斜杠结尾
 	siteStartDate: "2026-03-10", // 站点开始运行日期，用于站点统计组件计算运行天数
 
 	timeZone: SITE_TIMEZONE,
